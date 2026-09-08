f651559a631c3ce804c87a42af21979b0a6db98d
25sw0302-pixel <25sw0302@sonline20.sen.go.kr>
2026-09-08 11:21:28 +0900

    Add files via upload

---
+++ script_학생.js
@@ -0,0 +1,33 @@
+/* =========================================================
+   우리반 백과 - script.js
+   지금은 비어 있습니다.
+   2차시부터 이 파일에 '외관' 패널을 움직이는 코드를 넣습니다.
+   ========================================================= */
+
+   /* 글자 크기 변경 */
+var fontInputs = document.querySelectorAll('input[name="fontsize"]');
+
+fontInputs.forEach(function (input){
+  input.addEventListener("change", function (){
+    // <html> 태그의 data-fontsize 속성 값을 선택한 라디오 버튼의 value로 변경
+    document.documentElement.setAttribute("data-fontsize", input.value);
+  });
+});
+
+/* 너비 변경 */
+var widthInputs = document.querySelectorAll('input[name="width"]');
+
+widthInputs.forEach(function (input){
+  input.addEventListener("change", function (){
+    document.documentElement.setAttribute("data-width", input.value);
+  });
+});
+
+/* 색상 테마 변경 */
+var themeInputs = document.querySelectorAll('input[name="theme"]');
+
+themeInputs.forEach(function (input){
+  input.addEventListener("change", function (){
+    document.documentElement.setAttribute("data-theme", input.value);
+  });
+});
